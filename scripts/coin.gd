941b82ec9060e4a976f38d21c6a752458206abb6
yehuda <yehudagurevich@gmail.com>
2025-10-12 21:36:39 -0400

    something random

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
+@onready var game_manager: Node = %"game manager"
 
 
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
+	game_manager.add_point()
 	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -29,7 +29,18 @@ func _physics_process(delta):
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+		
+	#play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
 
+	
+	
 	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
